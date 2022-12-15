c4a7f2139901d3e38baaa5444f935914270917f0
Saito-Izo <kiyoharusaito@gmail.com>
2022-12-15 23:05:23 +0900

    Currency converter
    Currency converter

---
+++ main.py
@@ -0,0 +1,28 @@
+# importar o App, Builder (GUI)
+# criar aplicativo
+# criar a função build
+
+from kivy.app import App
+from kivy.lang import Builder
+import requests
+
+GUI = Builder.load_file("tela.kv")
+# class é uma estrutura de orientação a objeto
+class MeuAplicativo(App):
+    def build(self):
+        return GUI
+
+    def on_start(self):
+        self.root.ids["moeda1"].text = f"Dólar R$ {self.pegar_cotacao('USD')}"
+        self.root.ids["moeda2"].text = f"Euro R$ {self.pegar_cotacao('EUR')}"
+        self.root.ids["moeda3"].text = f"Iene R$ {self.pegar_cotacao('JPY')}"
+        self.root.ids["moeda4"].text = f"Dólar Australiano R$ {self.pegar_cotacao('AUD')}"
+
+    def pegar_cotacao(self, moeda):
+        link = f"https://economia.awesomeapi.com.br/last/{moeda}-BRL"
+        requisicao = requests.get(link)
+        dic_requisicao = requisicao.json()
+        cotacao = dic_requisicao[f"{moeda}BRL"]["bid"]
+        return cotacao
+
+MeuAplicativo().run()
